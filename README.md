513d591c5e1ac398da16d473a033f43382410a72
Jonathan A. McCormick, Jr. <67705789+JonathanMcCormickJr@users.noreply.github.com>
2025-03-13 18:39:32 -0500

    Add requirements

---
+++ README.md
@@ -0,0 +1,10 @@
+# PhishingSpider.com
+
+## Requirements
+1. A universal header with logo
+2. A universal footer with logo
+3. A home page
+4. An about page
+5. A contact/connect/follow page with links
+
+
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,16 @@ fn index() -> &'static str {
     "hello phish!!"
 }
 
+#[get("/about")]
+fn about() -> &'static str {
+    "<h1>About PhishingSpider</h1><br>Phishing Spider LLC is a security-conscious tech company based out of Bartlesville, Oklahoma."
+}
+
+
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, about])
 }
+
 
