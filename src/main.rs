ce60742188e2d04360cf45ad1723496c876b036e
Jonathan A. McCormick, Jr. <67705789+JonathanMcCormickJr@users.noreply.github.com>
2025-03-13 21:18:12 -0500

    MVP done

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,20 @@ fn index() -> Template {
     })
 }
 
+#[get("/about")]
+fn about() -> Template {
+    Template::render("about", context! {
+        title: "About"
+    })
+}
+
+#[get("/connect")]
+fn connect() -> Template {
+    Template::render("connect", context! {
+        title: "Connect"
+    })
+}
+
 #[catch(404)]
 fn four_o_four(req: &Request<'_>) -> Template {
     Template::render("error/404", context! {
@@ -19,7 +33,7 @@ fn four_o_four(req: &Request<'_>) -> Template {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index]).register("/", catchers![four_o_four]).attach(Template::fairing())
+    rocket::build().mount("/", routes![index, about, connect]).register("/", catchers![four_o_four]).attach(Template::fairing())
 }
 
 
